83caa221528111b39449ad50af95ce001c604514
mgberrysd <mgberrysd@gmail.com>
2024-02-28 12:28:18 -0800

    finished initinal typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,12 +1,36 @@
 const typeDefs = `
+  type User {
+    _id: ID
+    username: String
+    password: String
+    completedStory: [CompletedStory]
+  }
 
+  type UnfinishedStory {
+    _id: ID
+    unfinishedText: String
+    blanks: [String]!
+  }
+
+  type CompletedStory {
+    _id: ID
+    finishedText: String
+    user: User
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
 
   type Query {
-
+    user(userId: ID!): User
+    story(storyId: ID!): UnfinishedStory
   }
 
   type Mutation {
-
+    addUser(username: String!, password: String!): Auth
+    createStory(finishedText: String!): CompletedStory
   }
 `;
 
