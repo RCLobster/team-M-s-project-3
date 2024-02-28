2ff11c227e85fd70506ef757274433abfda30a71
RCLobster <44736025+RCLobster@users.noreply.github.com>
2024-02-28 14:25:50 -0800

    UnifinishedStory component, began work on StorySelector page

---
+++ client/src/components/UnfinishedStory.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import { useMutation, useQuery } from '@apollo/client';
+
+const UnfinishedStory = (title) => {
+
+    return (
+        <div>
+            <button className="btn" type="confirm">
+                {title}
+            </button>
+        </div>
+    )
+};
+
+export default UnfinishedStory;
