b038325540ce7baedcf734896291dd57dcbe0ad7
RCLobster <44736025+RCLobster@users.noreply.github.com>
2024-02-28 12:10:56 -0800

    more story seeds, user seeds

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,9 +1,19 @@
 const db = require('../config/connection');
+const { UnfinishedStory , CompletedStory, User } = require('../models');
 
 const storyData = require('./storySeeds.json');
 const userData = require('./userSeeds.json');
 
 db.once('open', async () => {
-    
+    await cleanDB('UnfinishedStory', 'unfinishedStories');
+    await cleanDB('CompletedStory', 'completedStories');
+    await cleanDB('User', 'users');
+
+    await User.create(userData);
+
+    await UnfinishedStory.create(storyData);
+
+    console.log("DB Seeded!");
+    process.exit(0);
 })
 
