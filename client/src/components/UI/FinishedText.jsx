533492c35a213c3c634e805b35cf8070cc02ca0e
RCLobster <44736025+RCLobster@users.noreply.github.com>
2024-02-29 14:30:50 -0800

    create story has cannot authenticate user problem

--- client/src/components/UI/FinishedText.jsx
+++
@@ -1,7 +0,0 @@
-const unfinishedText = "__ __ to the __ to get __";
-let finishedText = unfinishedText;
-const input = ["Jeff", "skeedaddled", "Walmart Super Center", "ammo"];
-input.forEach((word) => {
-     finishedText = finishedText.replace('__', word)
-});
-console.log(finishedText)
--- client/src/pages/CreateStory.jsx
+++ client/src/pages/CreateStory.jsx
@@ -1,18 +1,24 @@
-import { useQuery } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
 import { useParams } from 'react-router-dom';
 import { QUERY_SINGLE_UNFINISHED_STORY } from "../utils/queries";
+import { CREATE_STORY } from '../utils/mutations';
 import StoryBlanks from '../components/StoryBlanks';
+import Auth from '../utils/auth';
 
 const CreateStory = () => {
+    console.log(Auth.userId);
+
     const { storyId } = useParams();
     console.log(storyId);
     const { loading, data } = useQuery(QUERY_SINGLE_UNFINISHED_STORY, {
         variables: { storyId: storyId }
     });
+    
+    const [createStory, { error }] = useMutation(CREATE_STORY);
 
     const story = data?.unfinishedStory || [];
 
-    console.log(story.blanks);
+    let finishedText = story.unfinishedText;
 
     const handleClick = async (event) => {
         event.preventDefault();
@@ -20,8 +26,23 @@ const CreateStory = () => {
         console.log(event.target);
 
         let userInputs = document.getElementsByClassName("userInput");
-        console.log(userInputs);
-    }
+
+        for(let x=0; x<userInputs.length; x++) {
+            finishedText = finishedText.replace('__', userInputs[x].value) 
+        };
+
+        console.log(finishedText);
+
+        try {
+            const { data } = await createStory({
+                variables: { finishedText },
+            });
+        } catch (err) {
+            console.error(err);
+        };
+    };
+
+
 
     return (
         <div>
