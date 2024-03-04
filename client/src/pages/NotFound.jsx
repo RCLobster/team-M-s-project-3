789b0790c28235aa8738b2912af1f67f191db0ac
RCLobster <44736025+RCLobster@users.noreply.github.com>
2024-03-04 10:50:05 -0800

    created 404 page

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,6 +1,9 @@
 const NotFound = () => {
     return (
-        <div>Not Found page</div>
+        <div className="error-page">
+            <h2 className="error-page-title">404: ____ Not Found</h2>
+            <h4>Give me a noun: _<u>Page</u>_</h4>
+        </div>
     );
 };
 
