63addf6c9b0f2296fbb756e260d583eee5088b84
mgberrysd <mgberrysd@gmail.com>
2024-03-04 13:32:25 -0800

    removed redundent tts functions, removed react-speech package

--- client/src/components/CompletedStory.jsx
+++ client/src/components/CompletedStory.jsx
@@ -1,7 +1,7 @@
 import { useQuery } from "@apollo/client"
 import { QUERY_SINGLE_COMPLETED_STORY } from "../utils/queries"
-import Speech from 'react-speech';
-import speechUtteranceChunker from '../utils/chunkify';
+import { Button } from 'antd';
+
 
 export default function CompletedStory({ completeStoryId }) {
     const { loading, data } = useQuery(QUERY_SINGLE_COMPLETED_STORY, {
@@ -9,25 +9,40 @@ export default function CompletedStory({ completeStoryId }) {
     });
 
     const finishedStory = data?.completedStory.finishedText || "";
-    var utterance = new SpeechSynthesisUtterance(finishedStory);
-    var voiceArr = speechSynthesis.getVoices();
-    utterance.voice = voiceArr[2];
 
-    // pass it into the chunking function to have it played out.
-    // you can set the max number of characters by changing the chunkLength property below.
-    // a callback function can also be added that will fire once the entire text has been spoken.
-    speechUtteranceChunker(utterance, {
-        chunkLength: 120
-    }, function () {
-        //some code to execute when done
-        console.log('done');
-    });
+    const synth = window.speechSynthesis;
+    let voiceArr;
+    voiceArr = synth.getVoices();
+    if ("onvoiceschanged" in synth) {
+        synth.onvoiceschanged = synth.getVoices();
+    } else {
+        synth.getVoices()
+    }
+
+    const utterance = new SpeechSynthesisUtterance(finishedStory);
+
+    utterance.voice = voiceArr[6];
+
+    const speackVoice = async () => {
+        var myTimeout;
+
+        function myTimer() {
+            window.speechSynthesis.pause();
+            window.speechSynthesis.resume();
+            myTimeout = setTimeout(myTimer, 1000);
+
+        }
+
+        window.speechSynthesis.cancel();
+        myTimeout = setTimeout(myTimer, 1000);
+        utterance.onend = function () { clearTimeout(myTimeout); }
+        window.speechSynthesis.speak(utterance);
+    }
+
     return (
         <div className="finished-text">
             <p>{finishedStory}</p>
-            <Speech
-                text={finishedStory}
-                voice={voiceArr} />
+            <Button type="primary" onClick={speackVoice}>Hey listen!</Button>
         </div>
     );
 
--- client/src/components/CompletedStoryList.jsx
+++ client/src/components/CompletedStoryList.jsx
@@ -1,6 +1,4 @@
 import { Link } from 'react-router-dom';
-import Speech from 'react-speech';
-// import speechUtteranceChunker from '../utils/chunkify';
 import { Button } from 'antd';
 
 
@@ -19,98 +17,25 @@ const CompletedStoryList = ({
   }
 
   var utterance = new SpeechSynthesisUtterance(stories[0].finishedText);
-  console.log(utterance)
 
+  utterance.voice = voiceArr[6];
 
-  console.log(voiceArr)
-  
-  utterance.voice = voiceArr[4];
-
-  console.log(utterance.voice)
-
-
-  // function myTimer() {
-  //     window.speechSynthesis.pause();
-  //     window.speechSynthesis.resume();
-  //     myTimeout = setTimeout(myTimer, 10000);
-  // }
-
-
-  // var speechUtteranceChunker = function (utt, settings, callback) {
-  //   settings = settings || {};
-  //   var newUtt;
-  //   var txt = (settings && settings.offset !== undefined ? utt.text.substring(settings.offset) : utt.text);
-  //   if (utt.voice) { // Not part of the spec
-  //     newUtt = utt;
-  //     newUtt.text = txt;
-  //     newUtt.addEventListener('end', function () {
-  //       if (speechUtteranceChunker.cancel) {
-  //         speechUtteranceChunker.cancel = false;
-  //       }
-  //       if (callback !== undefined) {
-  //         callback();
-  //       }
-  //     });
-  //   }
-  //   else {
-  //     var chunkLength = (settings && settings.chunkLength) || 160;
-  //     var pattRegex = new RegExp('^[\\s\\S]{' + Math.floor(chunkLength / 2) + ',' + chunkLength + '}[.!?,]{1}|^[\\s\\S]{1,' + chunkLength + '}$|^[\\s\\S]{1,' + chunkLength + '} ');
-  //     var chunkArr = txt.match(pattRegex);
-
-  //     if (chunkArr[0] === undefined || chunkArr[0].length <= 2) {
-  //       //call once all text has been spoken...
-  //       if (callback !== undefined) {
-  //         callback();
-  //       }
-  //       return;
-  //     }
-  //     var chunk = chunkArr[0];
-  //     newUtt = new SpeechSynthesisUtterance(chunk);
-  //     var x;
-  //     for (x in utt) {
-  //       if (utt.hasOwnProperty(x) && x !== 'text') {
-  //         newUtt[x] = utt[x];
-  //       }
-  //     }
-  //     newUtt.addEventListener('end', function () {
-  //       if (speechUtteranceChunker.cancel) {
-  //         speechUtteranceChunker.cancel = false;
-  //         return;
-  //       }
-  //       settings.offset = settings.offset || 0;
-  //       settings.offset += chunk.length - 1;
-  //       speechUtteranceChunker(utt, settings, callback);
-  //     });
-  //   }
-
-  //   if (settings.modifier) {
-  //     settings.modifier(newUtt);
-  //   }
-  //   console.log(newUtt); //IMPORTANT!! Do not remove: Logging the object out fixes some onend firing issues.
-  //   //placing the speak invocation inside a callback fixes ordering and onend issues.
-  //   setTimeout(function () {
-  //     speechSynthesis.speak(newUtt);
-  //   }, 0);
-  // };
-
-  // const speech = async () => speechUtteranceChunker(utterance, {chunkLength: 120}, function () {console.log('done');})
-  const speech = async () => {
+  const speackVoice = async () => {
     var myTimeout;
 
     function myTimer() {
       window.speechSynthesis.pause();
       window.speechSynthesis.resume();
-      myTimeout = setTimeout(myTimer, 10000);
-      
-  }
-  
-  window.speechSynthesis.cancel();
-  myTimeout = setTimeout(myTimer, 10000);
-  utterance.onend =  function() { clearTimeout(myTimeout); }
-  window.speechSynthesis.speak(utterance);
+      myTimeout = setTimeout(myTimer, 1000);
+
+    }
+
+    window.speechSynthesis.cancel();
+    myTimeout = setTimeout(myTimer, 1000);
+    utterance.onend = function () { clearTimeout(myTimeout); }
+    window.speechSynthesis.speak(utterance);
   }
 
-  
   if (!stories.length) {
     return <h3>No Stories Yet</h3>;
   }
@@ -142,8 +67,8 @@ const CompletedStoryList = ({
             <div className="card-body bg-light p-2">
               <h2>Story</h2>
               <p>{story.finishedText}</p>
-              <Button type="primary" onClick={speech}>Submit All</Button>
-          </div>
+              <Button type="primary" onClick={speackVoice}>Hey listen!</Button>
+            </div>
             {/* <Link
               className="btn btn-primary btn-block btn-squared"
               to={`/story/${story._id}`}
@@ -151,8 +76,8 @@ const CompletedStoryList = ({
               Click here to view the whole story!
             </Link> */}
           </div>
-  ))
-}
+        ))
+      }
     </div >
   );
 };
